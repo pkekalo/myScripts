00751294090a00277adc3df8f60ea1cc28013929
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2018-12-21 20:53:07 +0100

    Make a magicalResistCalculator function

---
+++ dota_2/all/magicaDamage.js
@@ -0,0 +1,8 @@
+var withMagicalResist = function (magicalDamage, magicalResistancePercent) {
+    let clearDamage = magicalDamage * (1 - magicalResistancePercent/100);
+    let damage = clearDamage;
+    return  damage + " clear damage..."; 
+}
+//How it works...
+withMagicalResist(600, 25);
+//returns "450 clear damage...".
