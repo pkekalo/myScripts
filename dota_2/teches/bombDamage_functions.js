0e649500bcbb2b4dd064c813814091faa5116d20
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2018-12-21 18:23:05 +0100

    Create damage variable, add comments

--- dota_2/teches/bombDamage_functions.js
+++ dota_2/teches/bombDamage_functions.js
@@ -1,55 +1,58 @@
-//Different ult lvls functions
+// Initial damage is always 0 utill you do damage.
+var damage = 0;
+
+// Different Ult lvls functions. The higher lvl of the Ult the more damage it does.
 
 var minerUltLvl1 = function (numberOFMines, callback, magicalResistancePercent) {
-	var damage = numberOFMines * 300;
-	if (typeof callback === "function") {
-    	 return callback(damage, magicalResistancePercent);
+    var damage = numberOFMines * 300;
+    if (typeof callback === "function") {
+         return callback(damage, magicalResistancePercent);
     } else {
-    	return damage + " magical damage...";
+        return damage + " magical damage...";
     }
 }
 
 var minerUltLvl2 = function (numberOFMines, callback, magicalResistancePercent) {
-	var damage = numberOFMines * 450;
-	if (typeof callback === "function") {
-    	 return callback(damage, magicalResistancePercent);
+    var damage = numberOFMines * 450;
+    if (typeof callback === "function") {
+         return callback(damage, magicalResistancePercent);
     } else {
-    	return damage + " magical damage...";
+        return damage + " magical damage...";
     }
 }
 
 var minerUltLvl3 = function (numberOFMines, callback, magicalResistancePercent) {
-	var damage = numberOFMines * 600;
-	if (typeof callback === "function") {
-    	 return callback(damage, magicalResistancePercent);
+    var damage = numberOFMines * 600;
+    if (typeof callback === "function") {
+         return callback(damage, magicalResistancePercent);
     } else {
-    	return damage + " magical damage...";
+        return damage + " magical damage...";
     }
 }
 
 var minerUltLvl4 = function (numberOFMines, callback, magicalResistancePercent) {
     var damage = numberOFMines * 750;
     if (typeof callback === "function") {
-    	 return callback(damage, magicalResistancePercent);
+         return callback(damage, magicalResistancePercent);
     } else {
-    	return damage + " magical damage...";
+        return damage + " magical damage...";
     }
 }
 
 
-//callback function 
+// Callback function for minerUltLvl4(1-4) to calculate damage after resistance
 
 var withMagicalResist = function (magicalDamage, magicalResistancePercent) {
-	let clearDamage = magicalDamage * (1 - magicalResistancePercent/100);
-	damage = clearDamage;
-	return  damage + " clear damage..."; 
+    let clearDamage = magicalDamage * (1 - magicalResistancePercent/100);
+    damage = clearDamage;
+    return  damage + " clear damage..."; 
 }
 
 
 //How it works...
 
 minerUltLvl1(4, withMagicalResist, 25);
-//shuld return "900 clear damage...";
+//returns "900 clear damage...";
 
 minerUltLvl1(4);
-//shuld return "1200 magical damage...";
+//returns "1200 magical damage...";
